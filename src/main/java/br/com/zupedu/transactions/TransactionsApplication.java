86d65d13ec6d06ecc513ffb966071e93862655b6
Gleydson Miranda Souza <gleydson.m.s.1993@gmail.com>
2021-06-25 09:46:15 -0300

    Autenticação e Autorização

--- src/main/java/br/com/zupedu/transactions/TransactionsApplication.java
+++ src/main/java/br/com/zupedu/transactions/TransactionsApplication.java
@@ -2,8 +2,10 @@ package br.com.zupedu.transactions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class TransactionsApplication {
 
 	public static void main(String[] args) {
